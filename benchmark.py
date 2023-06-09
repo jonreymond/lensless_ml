49aa081d83b095d4c90e97e2002f667f61b5a4d8
root <root@iccluster146.iccluster.epfl.ch>
2023-06-09 09:01:48 +0000

    adapted benchmark

--- benchmark.py
+++ benchmark.py
@@ -3,7 +3,11 @@ import numpy as np
 import time
 import sys
 
-model_path = ''
+# with inversion
+# model_path = '/root/jreymond/lensless_ml/outputs/2023-06-06/06-56-56/tensorflow/models/wallerlab_unet.pb'
+# without_inversion
+model_path = '/root/jreymond/lensless_ml/outputs/2023-06-02/10-40-15/tensorflow/models/wallerlab_unet.pb'
+
 use_gpu = False
 
 
@@ -22,12 +26,19 @@ import keras
 
 if __name__ == "__main__":
 
+    model = tf.saved_model.load(model_path)
 
-    model = keras.models.load_model(model_path)
-
-    test_sample = np.random.rand(1, 304, 432, 4).astype(np.float32)
-
-    start_time = time.time()
+    # shape: num_samples=1, height=270, width=480, channels=3
+    shape = (1, 270, 480, 3)
+    # should be between [-1, 1]
+    test_sample = np.random.rand(*shape).astype(np.float32)
+    # warmup, output shape = (1, 210, 380, 3)
+    # output : between [-1, 1]
     output = model(test_sample)
-    runtime = (time.time() - start_time) * 1000
-    print('time: {:.3f} ms'.format(runtime))
+    
+    for i in range(10):
+        test_sample = np.random.rand(*shape).astype(np.float32) * 2 - 1
+        start_time = time.time()
+        output = model(test_sample)
+        runtime = (time.time() - start_time) * 1000
+        print('time: {:.3f} ms'.format(runtime))
